ba124d97372db5b8f26e6ffa83df7bec7d0a185f
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2022-09-06 08:11:17 +0000

    :pencil2: add bot re-export

--- nonebot/adapters/github/__init__.py
+++ nonebot/adapters/github/__init__.py
@@ -1,6 +1,8 @@
 from .event import *
 from .bot import Bot as Bot
+from .bot import OAuthBot as OAuthBot
 from .adapter import Adapter as Adapter
+from .bot import GitHubBot as GitHubBot
 from .message import Message as Message
 from .exception import ActionFailed as ActionFailed
 from .exception import NetworkError as NetworkError
